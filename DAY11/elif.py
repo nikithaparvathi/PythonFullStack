ee6d9a3b53982a7687651180fa68cd5da3077093
Nikitha Parvathi <nikithaparvathi@gmail.com>
2026-08-08 16:02:07 +0530

    elif.py

---
+++ DAY11/elif.py
@@ -0,0 +1,24 @@
+stock = 5 
+if stock > 20: 
+   print("Stock is fully available") 
+elif stock > 0: 
+     print("Limited stock available") 
+else: 
+    print("Out of stock") 
+
+
+
+
+
+
+stock = 10 
+premium_member = True 
+if stock > 0: 
+   print("Product is available") 
+   if premium_member: 
+      print("Priority delivery available") 
+   else: 
+         print("Standard delivery available") 
+else: 
+    print("Product is out of stock") 
+   
