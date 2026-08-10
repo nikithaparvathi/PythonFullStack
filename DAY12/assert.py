f36cc6f029229ddb7d7ba3294d60c1bbe8ae3b3a
Nikitha Parvathi <nikithaparvathi@gmail.com>
2026-08-10 21:07:07 +0530

    while

---
+++ DAY12/assert.py
@@ -0,0 +1,4 @@
+age = 18
+assert age >= 18,  "not eligible for vote due to age"
+print("eligible for vote")
+
---
+++ DAY12/forelse.py
@@ -0,0 +1,9 @@
+id =[101,102,103, 104]
+key = 108
+for i in range(len(id)):
+    if id [i] == key:
+        print("key is found")
+        break
+else:
+    print("key is not found")
+print("end")      
