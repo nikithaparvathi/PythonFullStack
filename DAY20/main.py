c08784194a1127c3b396dbafed6fd8f30df349b4
Nikitha Parvathi <nikithaparvathi@gmail.com>
2026-08-19 19:31:52 +0530

    calculator.py

---
+++ DAY20/main.py
@@ -0,0 +1,5 @@
+import calculator
+print("Addition:", calculator.add(10,5))
+print("subtraction:",calculator.sub(10,5))
+print("Multiplication:", calculator.mul(10,5))
+print("Division:", calculator.div(10,5))
